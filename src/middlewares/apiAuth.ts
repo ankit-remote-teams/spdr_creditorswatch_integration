e294623f6e76fd28cf06857ac5e83e67ac9c15ac
Ankit Sharma <ankit@remote-teams.com.au>
2024-10-24 11:18:53 +0530

    Fixing the required

--- src/middlewares/apiAuth.ts
+++ src/middlewares/apiAuth.ts
@@ -2,14 +2,14 @@ import { Request, Response, NextFunction } from 'express';
 
 const apiKey = process.env.AUTH_API_KEY || "";
 
-console.log('apiKey', apiKey);
-console.log('proveces auth pai ', process.env.AUTH_API_KEY)
+
 const VALID_API_KEYS: string[] = [apiKey];
 
 // Middleware function to authenticate API key
 export const apiKeyAuth = (req: Request, res: Response, next: NextFunction): void => {
     const apiKey = req.headers['x-api-key'] as string;
-
+    console.log('apiKey', apiKey);
+    console.log('proveces auth pai ', process.env.AUTH_API_KEY)
     if (!apiKey) {
         res.status(401).json({ error: 'API key is missing' });
         return;
