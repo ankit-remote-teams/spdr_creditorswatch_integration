ca52f9f5b5adeecf58ad0f3f381faf7857a724b7
Ankit Sharma <ankit@remote-teams.com.au>
2024-11-08 10:41:10 +0530

    Fixing the issue related with rate limiting

--- src/cron/deleteDataScheduler.ts
+++ src/cron/deleteDataScheduler.ts
@@ -9,51 +9,59 @@ import ContactMappingModel from '../models/contactMappingModel';
 import CreditNoteMappingModel from "../models/creditNotesMappingModel";
 import { ses } from '../config/awsConfig'
 
+// Delay function to pause execution for a given time in milliseconds
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
 export const handleDeleteContactScheduler = async () => {
     try {
-        console.log('cam her 1')
         const contactMappingData: MappingType[] = await ContactMappingModel.find({});
         let contactSimproIdArray: string[] = contactMappingData.map(contactSimpro => contactSimpro.simproId);
         let simproIdToUpdateAsDeleted: string[] = [];
-        console.log('cam her 3')
         for (let i = 0; i < contactSimproIdArray.length; i++) {
             try {
+                await delay(100);
+                // Make an API call to get individual contact details
                 let individualContactResponse = await axiosSimPRO.get(`/customers/companies/${contactSimproIdArray[i]}?columns=ID,CompanyName,Email,Archived`);
-                if (individualContactResponse.data) {
-                    if (individualContactResponse.data?.Archived) {
-                        simproIdToUpdateAsDeleted.push(individualContactResponse.data.ID)
-                    }
+
+                // Check if contact data is available and archived
+                const contactData = individualContactResponse.data;
+                if (contactData && contactData.Archived) {
+                    console.log("Simpro contact marked as delete : ", contactData.ID)
+                    simproIdToUpdateAsDeleted.push(contactSimproIdArray[i]);
                 }
-                console.log('cam her 2')
             } catch (error) {
-                console.log("ERROR 1", error)
-                console.log("Error in delete contact scheduler: ", error)
+                console.log("Error in delete contact scheduler: ", error);
+
+                // Handle Axios errors
                 if (error instanceof AxiosError) {
+                    if (error.response?.status === 429) {
+                        console.log('Rate limit reached. Retrying...');
+                        await delay(1000); // Delay before retry
+                        i--;  // Decrement index to retry the same contact
+                        continue;
+                    }
+
+                    // Handle specific 'Company customer not found' error
                     const errors = error?.response?.data?.errors;
-                    if (errors && errors.length > 0 && errors[0]?.message === "Company customer not found.") {
-                        let url: string = error?.request?.path;
-                        if (url) {
-                            const parts = url.split('/');
-                            let customerId = "";
-                            if (parts && parts.length > 0) {
-                                const lastPart = parts[parts.length - 1];
-                                customerId = lastPart ? lastPart.split('?')[0] : "";
-                            }
-                            if (customerId) {
-                                simproIdToUpdateAsDeleted.push(customerId);
-                            }
+                    if (errors?.[0]?.message === "Company customer not found.") {
+                        const url: string = error.request?.path || "";
+                        const parts = url.split('/');
+                        const customerId = parts.length ? parts[parts.length - 1].split('?')[0] : "";
+
+                        if (customerId) {
+                            simproIdToUpdateAsDeleted.push(customerId);
                         }
                     } else {
-                        console.log('DELETE SCHEDULER : Unexpected AxiosError in Individual:', error);
+                        console.log('DELETE SCHEDULER: Unexpected AxiosError in Individual:', error);
                         throw error;
                     }
                 } else {
-                    console.log('DELETE SCHEDULER : Unexpected error in Individual:', error);
+                    console.log('DELETE SCHEDULER: Unexpected error in Individual:', error);
                     throw error;
                 }
             }
-
         }
+
 
         let creditorWatchContactIdArray: string[] = contactMappingData
             .filter(mappingItem => simproIdToUpdateAsDeleted.includes(mappingItem.simproId))
@@ -61,13 +69,13 @@ export const handleDeleteContactScheduler = async () => {
 
         for (let creditorsId of creditorWatchContactIdArray) {
             try {
+                await delay(100);
                 const response = await creditorsWatchPutWithRetry(`/contacts/${creditorsId}`, { contact: { status: 'deleted' } })
                 if (!response) {
                     console.log('DELETE SCHEDULER : Failed to update contact data after multiple attempts.');
                     continue;
                 }
             } catch (error) {
-                console.log("ERROR 2", error)
                 console.log("DELETE SCHEDULER : Failed to update delete data catch 2", error)
                 if (error instanceof AxiosError) {
                     console.log('DELETE SCHEDULER : Error deleting contact data:', error.response?.data || error.message);
@@ -80,10 +88,9 @@ export const handleDeleteContactScheduler = async () => {
 
         }
 
-        console.log("DELETE SCHEDULER : Completed")
+        console.log("DELETE SCHEDULER : Completed for Contacts")
 
     } catch (error) {
-        console.log("ERROR 3", error)
         if (error instanceof AxiosError) {
             console.log('DELETE SCHEDULER : Error deleting contact data:', error.response?.data || error.message);
             throw { message: error.message, data: error?.response?.data }
@@ -102,51 +109,55 @@ export const handleDeleteInvoiceScheduler = async () => {
 
         for (let i = 0; i < invoiceSimproIdArray.length; i++) {
             try {
+                await delay(100);
                 let individualInvoiceResponse = await axiosSimPRO.get(`/invoices/${invoiceSimproIdArray[i]}?columns=ID,Customer,Stage`);
-                if (individualInvoiceResponse.data) {
-                    if (individualInvoiceResponse.data?.ID) {
-                        continue;
-                    }
+
+                // Check if invoice data is valid
+                if (individualInvoiceResponse.data?.ID) {
+                    continue;
+                } else {
+                    console.log("Simpro invoice marked as delete : ", invoiceSimproIdArray[i])
+                    simproIdToUpdateAsDeleted.push(invoiceSimproIdArray[i]);
                 }
+
             } catch (error) {
                 if (error instanceof AxiosError) {
-                    console.log("ERROR 4", error?.response?.data)
-                    if (error?.response?.data?.error == "Invalid resource URI. The correct URI is specified in _href.") {
+                    // Handle rate limit errors with retry
+                    if (error.response?.status === 429) {
+                        console.log('Rate limit reached. Retrying...');
+                        await delay(1000);  // Wait before retrying
+                        i--;  // Retry the current request
+                        continue;
+                    }
+
+                    // Handle specific "Invalid resource URI" error
+                    if (error?.response?.data?.error === "Invalid resource URI. The correct URI is specified in _href.") {
                         try {
                             const fullHref = error.response.data._href;
-        
-                            // Remove the base URL and company path if they exist in _href, leaving just the relative path.
-                            const relevantPath = fullHref.replace(/^\/api\/v1\.0\/companies\/2\//, '');
-                            let retainageId = relevantPath.split('/')[relevantPath.split('/').length - 1];
-                    
-                            // Make the request with the corrected path
+                            const relevantPath = fullHref.replace(/^\/api\/v1\.0\/companies\/2/, '');
+                            const retainageId = relevantPath.split('/').pop();
+
+                            // Correct the request path and retry
                             const responseRetainage = await axiosSimPRO.get(relevantPath);
-                            if(!responseRetainage.data){
+                            if (!responseRetainage.data) {
                                 simproIdToUpdateAsDeleted.push(retainageId);
                             }
-                        }
-                        catch (error) {
-                            if (error instanceof AxiosError) {
-                                console.log('DELETE SCHEDULER : Error fetching retainage :', error.response?.data || error.message);
-                                throw { message: error.message, data: error?.response?.data }
+                        } catch (retainageError) {
+                            if (retainageError instanceof AxiosError) {
+                                console.log('DELETE SCHEDULER : Error fetching retainage:', retainageError.response?.data || retainageError.message);
+                                throw { message: retainageError.message, data: retainageError?.response?.data };
                             } else {
-                                console.log('DELETE SCHEDULER : Unexpected error fetch retainage:', error);
-                                throw error;
+                                console.log('DELETE SCHEDULER : Unexpected error fetch retainage:', retainageError);
+                                throw retainageError;
                             }
                         }
-                    } else if (error?.response?.data?.errors[0]?.message == "Invoice(s) not found.") {
-                        let url: string = error?.request?.path;
-                        if (url) {
-                            const parts = url?.split('/');
-                            let invoiceId: string = "";
-                            if (parts && parts.length > 0) {
-                                const lastPart = parts[parts.length - 1];
-                                invoiceId = lastPart ? lastPart.split('?')[0] : "";
-                            }
+                    } else if (error?.response?.data?.errors[0]?.message === "Invoice(s) not found.") {
+                        // Handle "Invoice(s) not found" error
+                        const url: string = error?.request?.path || "";
+                        const invoiceId = url.split('/').pop()?.split('?')[0] || "";
 
-                            if (invoiceId) {
-                                simproIdToUpdateAsDeleted.push(invoiceId);
-                            }
+                        if (invoiceId) {
+                            simproIdToUpdateAsDeleted.push(invoiceId);
                         }
                     } else {
                         console.log('DELETE SCHEDULER : Unexpected AxiosError in Individual:', error);
@@ -157,7 +168,6 @@ export const handleDeleteInvoiceScheduler = async () => {
                     throw error;
                 }
             }
-
         }
 
         let creditorWatchInvoiceIdArray: string[] = invoiceMappingData
@@ -166,13 +176,13 @@ export const handleDeleteInvoiceScheduler = async () => {
 
         for (let creditorsId of creditorWatchInvoiceIdArray) {
             try {
+                await delay(100)
                 const response = await creditorsWatchPutWithRetry(`/invoices/${creditorsId}`, { invoice: { status: 'deleted' } })
                 if (!response) {
                     console.log('DELETE SCHEDULER : Failed to update invoice data after multiple attempts.');
                     continue;
                 }
             } catch (error) {
-                console.log("ERROR 5", error)
                 if (error instanceof AxiosError) {
                     console.log('DELETE SCHEDULER : Error deleting invoice data:', error.response?.data || error.message);
                     throw { message: 'Error from Axios request', details: error.response?.data }
@@ -184,10 +194,9 @@ export const handleDeleteInvoiceScheduler = async () => {
 
         }
 
-        console.log("DELETE SCHEDULER : Completed")
+        console.log("DELETE SCHEDULER : Completed Invoice")
 
     } catch (error) {
-        console.log("ERROR 6", error)
         if (error instanceof AxiosError) {
             console.log('DELETE SCHEDULER : Error deleting invoice data:', error.response?.data || error.message);
             throw { message: error.message, data: error?.response?.data }
@@ -198,6 +207,7 @@ export const handleDeleteInvoiceScheduler = async () => {
     }
 }
 
+
 export const handleDeleteCreditNoteScheduler = async () => {
     try {
         const creditNoteMappingData: MappingType[] = await CreditNoteMappingModel.find({});
@@ -206,33 +216,33 @@ export const handleDeleteCreditNoteScheduler = async () => {
 
         for (let i = 0; i < creditNoteSimproIdArray.length; i++) {
             try {
+                await delay(100);
+
                 let individualCreditNoteResponse = await axiosSimPRO.get(`/creditNotes/${creditNoteSimproIdArray[i]}?columns=ID,Type,Customer,DateIssued,Stage,Total,InvoiceNo`);
-                if (individualCreditNoteResponse.data) {
-                    if (individualCreditNoteResponse.data?.ID) {
-                        continue;
-                    }
+
+                if (individualCreditNoteResponse.data?.ID) {
+                    continue;
                 }
             } catch (error) {
-                console.log("ERROR 7", error)
                 if (error instanceof AxiosError) {
+                    if (error.response?.status === 429) {
+                        console.log('Rate limit reached. Retrying...');
+                        await delay(1000);
+                        i--;
+                        continue;
+                    }
+
                     const errorMessage = error?.response?.data?.errors[0]?.message || "";
-
                     const creditNoteNotFoundRegex = /Credit\s*Note\s*#?\d*\s*not\s*found/i;
+
+                    // Handle "Credit Note not found" error
                     if (creditNoteNotFoundRegex.test(errorMessage)) {
-                        let url: string = error?.request?.path;
-                        if (url) {
-                            const parts = url?.split('/');
-                            let creditNoteId: string = "";
-                            if (parts && parts.length > 0) {
-                                const lastPart = parts[parts.length - 1];
-                                creditNoteId = lastPart ? lastPart.split('?')[0] : "";
-                            }
+                        const url: string = error?.request?.path || "";
+                        const creditNoteId = url.split('/').pop()?.split('?')[0] || "";
 
-                            if (creditNoteId) {
-                                simproIdToUpdateAsDeleted.push(creditNoteId);
-                            }
+                        if (creditNoteId) {
+                            simproIdToUpdateAsDeleted.push(creditNoteId);
                         }
-
                     } else {
                         console.log('DELETE SCHEDULER : Unexpected AxiosError in Individual:', error);
                         throw error;
@@ -242,7 +252,6 @@ export const handleDeleteCreditNoteScheduler = async () => {
                     throw error;
                 }
             }
-
         }
 
         let creditorWatchCreditNoteIdArray: string[] = creditNoteMappingData
@@ -251,37 +260,36 @@ export const handleDeleteCreditNoteScheduler = async () => {
 
         for (let creditorsId of creditorWatchCreditNoteIdArray) {
             try {
-                const response = await creditorsWatchPutWithRetry(`/credit_notes/${creditorsId}`, { credit_note: { status: 'deleted' } })
+                await delay(100);
+                const response = await creditorsWatchPutWithRetry(`/credit_notes/${creditorsId}`, { credit_note: { status: 'deleted' } });
                 if (!response) {
                     console.log('DELETE SCHEDULER : Failed to update credit note data after multiple attempts.');
                     continue;
                 }
             } catch (error) {
-                console.log("ERROR 8", error)
                 if (error instanceof AxiosError) {
                     console.log('DELETE SCHEDULER : Error deleting credit note data:', error.response?.data || error.message);
-                    throw { message: 'Error from Axios request', details: error.response?.data }
+                    throw { message: 'Error from Axios request', details: error.response?.data };
                 } else {
                     console.log('DELETE SCHEDULER : Unexpected error 5:', error);
                     throw error;
                 }
             }
-
         }
 
-        console.log("DELETE SCHEDULER : Completed")
+        console.log("DELETE SCHEDULER : Completed Credit Note");
 
     } catch (error) {
-        console.log("ERROR 9", error)
         if (error instanceof AxiosError) {
             console.log('DELETE SCHEDULER : Error deleting credit note data:', error.response?.data || error.message);
-            throw { message: error.message, data: error?.response?.data }
+            throw { message: error.message, data: error?.response?.data };
         } else {
             console.log('DELETE SCHEDULER : Unexpected error 6:', error);
             throw error;
         }
     }
-}
+};
+
 
 
 console.log('DELETE SCHEDULER : Delete contact scheduler time', moment().format('YYYY-MM-DD HH:mm:ss'))
